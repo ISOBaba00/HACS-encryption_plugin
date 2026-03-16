2d2ded8ad7d9f18b5d5a4ba9c1b021723482d7cb
ISOBaba00 <135527515+ISOBaba00@users.noreply.github.com>
2026-03-17 02:48:35 +0300

    1.0.3

--- custom_components/encryption_plugin/__init__.py
+++ custom_components/encryption_plugin/__init__.py
@@ -4,16 +4,18 @@ from .const import DOMAIN, CONF_CODE
 
 _LOGGER = logging.getLogger(__name__)
 
+async def async_setup(hass: HomeAssistant, config: dict):
+    """YAML üzerinden kurulum (boş bırakabiliriz)."""
+    return True
+
 async def async_setup_entry(hass: HomeAssistant, entry):
-    """Kurulum arayüzünden gelen verilerle eklentiyi başlatır."""
-    
-    # Kurulumda girilen şifreyi alıyoruz
+    """Arayüz (UI) üzerinden kurulum yapıldığında çalışır."""
     sifre = entry.data.get(CONF_CODE)
 
     async def verify_code_service(call: ServiceCall):
         girilen = call.data.get("entered_code")
         if girilen == sifre:
-            _LOGGER.info("Şifre Doğru! Varlık tetikleniyor...")
+            _LOGGER.info("Şifre Doğru! Eylem tetikleniyor...")
             hass.bus.async_fire("encryption_plugin_success")
         else:
             _LOGGER.warning("Hatalı şifre!")
